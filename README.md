2baa6d1d4181d6cf34ebeaffa32798edd9bafa48
Roee Shoshani <roysho10@gmail.com>
2023-11-18 03:26:07 +0200

    add documentation

---
+++ README.md
@@ -0,0 +1,4 @@
+# zydis-sys
+
+low level rust bindings for the zydis crate for encoding/decoding and messing with x86 instructions.
+for higher level bindings, check out the `rydis` crate.
--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,6 @@
+//! low level rust bindings for the zydis crate for encoding/decoding and messing with x86 instructions.
+//! for higher level bindings, check out the `rydis` crate.
+
 #![allow(non_upper_case_globals)]
 #![allow(non_snake_case)]
 #![allow(non_camel_case_types)]
