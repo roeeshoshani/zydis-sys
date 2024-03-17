f5c6061f4d549d0471ab3e9048872450d6792312
Roee Shoshani <roysho10@gmail.com>
2024-03-17 15:09:54 +0200

    add no_std

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,7 @@
 //! low level rust bindings for the zydis library for encoding/decoding and messing with x86 instructions.
 //! for higher level bindings, check out the `rydis` crate.
 
+#![no_std]
 #![allow(non_upper_case_globals)]
 #![allow(non_snake_case)]
 #![allow(non_camel_case_types)]
